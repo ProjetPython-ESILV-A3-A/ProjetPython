0c830b02b26871aa8e23e5233bc4d5f517a65c7a
TowerOfFailure <60489673+TowerOfFailure@users.noreply.github.com>
2020-05-07 18:33:55 +0200

    Modele provuit v1

--- mysite/habitant/models.py
+++ mysite/habitant/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Produit(models.Model):
+    nomProduit=models.CharField(max_length=40)
+    quantitemax=models.IntegerField()
+    prix=models.IntegerField()
+    def __str__(self):
+        return nomProduit
+    def allstr(self):
+        return nomProduit + " "+quantitemax+" /personnes, prix : " +prix
