5dcb4f41182ab97ed0977849873ee5b7580df6fb
Yassine-ABAAB <64488752+Yassine-ABAAB@users.noreply.github.com>
2020-05-07 17:46:46 +0200

    SQL V2

--- Creation Tables_projetpython.sql
+++ Creation Tables_projetpython.sql
@@ -4,19 +4,18 @@ create table `projetpython`.`Demandeur`(
 `id` int not null auto_increment,
 `nom` varchar(25) not null,
 `email` varchar (35) not null,
-`mdp` varchar(15) not null,
+`mdp` varchar(40) not null,
 `telephone` varchar(10) not null,
 `nbprochesfoyer` int not null,
 `nomsproches` varchar(150) not null,
 `adresse` varchar(60) not null,
-`argent` double not null,
 primary key (`id`));
 
 create table `projetpython`.`Admin`(
 `id` int not null auto_increment,
 `nom` varchar(25) not null,
 `email` varchar(35) not null,
-`mdp` varchar(15) not null,
+`mdp` varchar(40) not null,
 primary key (`id`));
 
 create table `projetpython`.`Produit`(
@@ -24,10 +23,5 @@ create table `projetpython`.`Produit`(
 `nom` varchar(25) not null,
 `categorie` varchar(25) not null,
 `unite` varchar(5) not null,
-`stock` int not null,
 `prix` double not null, 
 primary key (`id`));
-
-create table `projetpython`.`Entreprise`(
-`nom` varchar(25) not null,
-`argent` double not null );
