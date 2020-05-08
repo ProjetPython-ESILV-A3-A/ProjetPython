6fe1f17c348290dc1c3e6d6f0632197e9d7185c6
Yassine-ABAAB <64488752+Yassine-ABAAB@users.noreply.github.com>
2020-05-08 17:08:26 +0200

    SQL-Python-Tout type de requete

--- Connexion python-sql.py
+++ Connexion python-sql.py
@@ -5,8 +5,10 @@ from secret import bdd_login, bdd_password
 #Pour faire ca, allez dans cmd et tapez "pip install mysql-connector-python"
 #Et ensuite on l'importe dans la ligne d'en dessous
 import mysql.connector
+from mysql.connector import Error
+from mysql.connector import errorcode
 
-def ConnexionSQL(requete):
+def ConnexionSQLSelect(requete):
     madb = mysql.connector.connect(
         host = "localhost",
         user = bdd_login,
@@ -24,9 +26,56 @@ def ConnexionSQL(requete):
     
     return infos
 
-infos = ConnexionSQL("select email from Demandeur")
+def ConnexionSQL(requete):
+    try:
+        madb = mysql.connector.connect(
+            host = "localhost",
+            user = bdd_login,
+            passwd = bdd_password,
+            database = "projetpython"
+        
+        )
+    
+        moncurseur = madb.cursor()
+        moncurseur.execute(requete)
+        madb.commit()
+        print(moncurseur.rowcount, "tuple à bien été pris en compte")
+        moncurseur.close()
+    
+    except mysql.connector.Error as error:
+        print("La requete a echoue {}".format(error))
+
+    finally:
+        if (madb.is_connected()):
+            madb.close()
+            print("La connexion MYSQL est fermé")
+
+
+
+
+
+#TEST POUR LE INSERT/DELETE/UPDATE
+requeteInsert = "INSERT INTO `projetpython`.`Admin` (`nom`,`email`,`mdp`) VALUES ('yass','yass@gmail.com','monmdp')"
+requeteDelete = "DELETE from Admin where id = 3"
+requeteUpdate = "UPDATE Admin SET nom = 'Equipe 4' where id=1"
+#ConnexionSQL(requeteUpdate)
+
+
+#TEST POUR LE SELECT
+#requeteSelect = "select nom,id from Admin"
+#infos = ConnexionSQLSelect(requeteSelect)
+#for ligne in infos:
+#    print(ligne)
+
+#TEST AVEC VARIABLES
+nouveaunom = "Equipe 4"
+requeteSelect = "Select nom,id,email from Admin where nom = '"+nouveaunom+"'"
+infos = ConnexionSQLSelect(requeteSelect)
+
 for ligne in infos:
     print(ligne)
+    
+
 
 
 
