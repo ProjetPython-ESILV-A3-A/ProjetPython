efc239bec2c3a966151113300508066a95b26d09
Nandy Bâ <nandyba@yahoo.fr>
2020-05-22 18:16:12 +0200

    Adaptation de la page d'accueil

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -4,5 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def acceuil(request):
-    output=", ".join(["Page d'acceuil","acces Commune","acces Habitant"])
-    return HttpResponse(output)
+    return render(request, 'mysite/index.html')
